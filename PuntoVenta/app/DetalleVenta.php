a4bd4a86d5b47065b1d22a3260a216c6f706020f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-04-10 00:22:52 -0500

    Modelo y request: Venta y detalle de venta
     Cree los modelos de venta y detalle de venta con su request.

--- PuntoVenta/app/DetalleVenta.php
+++ PuntoVenta/app/DetalleVenta.php
@@ -4,20 +4,20 @@ namespace PuntoVenta;
 
 use Illuminate\Database\Eloquent\Model;
 
-class DetalleIngreso extends Model
+class DetalleVenta extends Model
 {
-    protected $table='detalle_ingreso';
+    protected $table='detalle_venta';
 
-    protected $primaryKey='iddetalle_ingreso';
+    protected $primaryKey='iddetalle_venta';
 
     public $timestamps = false;
 
     protected $fillabel = [
-    	'idingreso',
+    	'idventa',
         'idarticulo',
         'cantidad',
-        'precio_compra',
-        'precio_venta'
+        'precio_venta',
+        'descuento'
     ];
 
     protected $guarded =[
