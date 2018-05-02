c91ba9bef59515f38d70ece7a00a2543df3d68b0
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-05-01 19:41:48 -0500

    Gestion de Accesos 1-2
     Aun tengo problemas con la redireccion de login y al registrar, tambien
     creo que no se creo una tabla en la base de datos al usar el migrate.
     Otro punto a checar es que no le puse la proteccion de auth a todos los
     controllers

--- PuntoVenta/routes/web.php
+++ PuntoVenta/routes/web.php
@@ -22,4 +22,4 @@ Route::resource('compras/proveedor','ProveedorController');
 Route::resource('compras/ingreso','IngresoController');
 Auth::routes();
 
-Route::get('/compras/ingreso', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
