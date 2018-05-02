3ca614296502a78410983cd1368d262235cd627c
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-05-01 21:56:35 -0500

    Modelo, controlar y request de usuarios

---
+++ PuntoVenta/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace PuntoVenta\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use PuntoVenta\Http\Requests;
+
+use PuntoVenta\User;
+use Illuminate\Support\Facades\Redirect;
+use PuntoVenta\Http\Requests\UsuarioFormRequest;
+use DB;
+
+class UsuarioController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index(Request $request)
+    {
+        if ($request)
+        {
+            $query=trim($request->get('searchText'));
+            $usuarios=DB::table('users')->where('name','LIKE','%'.$query.'%')
+                ->orderBy('id','desc')
+                ->paginate(7);
+            return view('seguridad.usuario.index' ,["usuarios"=>$usuarios,"searchText"=>$query]);
+        }
+    }
+
+    public function create()
+    {
+        return view("seguridad.usuario.create");
+    }
+
+    public function store (UsuarioFormRequest $request)
+    {
+        $usuario=new User;
+        $usuario->name=$request->get('name');
+        $usuario->email=$request->get('email');
+        $usuario->password=bcrypt($request->get('password'));
+        $usuario->save();
+        return Redirect::to('seguridad/usuario');
+    }
+
+    public function show($id) {
+        return view("seguridad.usuario.show",["usuario"=>User::findOrFail($id)]);
+    }
+
+    public function edit($id)
+    {
+        return view("seguridad.usuario.edit" ,["usuario"=>User::findOrFail($id)]);
+    }
+
+    public function update(UsuarioFormRequest $request,$id)
+    {
+        $usuario=User::findOrFail($id);
+        $usuario->name=$request->get('name');
+        $usuario->email=$request->get('email');
+        $usuario->password=bcrypt($request->get('password'));
+        $usuario->update();
+        return Redirect::to('seguridad/usuario');
+    }
+
+    public function destroy($id)
+    {
+        $usuario=DB::table('users')->where('id','=',$id)->delete();
+        return Redirect::to('seguridad/usuario');
+    }
+}
